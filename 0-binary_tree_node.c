bfdcef0d21577ba8b1911fb9cb9597c665bca50f
Yves  Ishimwe <yves.ishimwe@amalitech.org>
2024-05-16 10:54:24 +0200

    Binary Trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,19 +8,19 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-    /* allocate memory for new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* allocate memory for new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* initialize new node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	/* initialize new node */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    /* return new node */
-    return (new_node);
+	/* return new node */
+	return (new_node);
 }
