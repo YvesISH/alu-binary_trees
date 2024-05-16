960b8e0b7e26581add410f93af637dd092d6e044
FrankOnyemaOrji <f.orji@alustudent.com>
2024-05-17 00:01:30 +0200

    Corrected betty error

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,23 +1,47 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * binary_trees_ancestor - finds the lowest ancestor
  * @first: pointer to the first node
  * @second: pointer to the second node
- * Return: pointer to the lowest common ancestor node, or NULL if not found
+ * Return: pointer to the lowest ancestor, or NULL
  * Author: Frank Onyema Orji
  */
-
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second)
 {
-	if (first == NULL || second == NULL)
+	const binary_tree_t *tmp;
+	int depth_first = 0, depth_second = 0;
+
+	if (!first || !second)
 		return (NULL);
 
-	if (first == second)
-		return ((binary_tree_t *)first);
+	tmp = first;
+	while (tmp)
+	{
+		depth_first++;
+		tmp = tmp->parent;
+	}
 
-	if (first->parent == second || first == second->parent)
-		return ((binary_tree_t *)(first->parent));
+	tmp = second;
+	while (tmp)
+	{
+		depth_second++;
+		tmp = tmp->parent;
+	}
 
-	return (binary_trees_ancestor(first->parent, second->parent));
+	for (; depth_first > depth_second; depth_first--)
+		first = first->parent;
+	for (; depth_second > depth_first; depth_second--)
+		second = second->parent;
+
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+
+	return (NULL);
 }
