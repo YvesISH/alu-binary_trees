114cfd337bfc3116b98bae0788bef924c33b32ab
root <y.ishimwe2@alustudent.com>
2024-05-17 00:52:06 -0700

    Binary Tree

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,29 +8,29 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *new_root;
+	binary_tree_t *new_root;
 
-    if (tree == NULL || tree->left == NULL)
-        return (tree);
+	if (tree == NULL || tree->left == NULL)
+		return (tree);
 
-    new_root = tree->left;
-    tree->left = new_root->right;
+	new_root = tree->left;
+	tree->left = new_root->right;
 
-    if (new_root->right != NULL)
-        new_root->right->parent = tree;
+	if (new_root->right != NULL)
+		new_root->right->parent = tree;
 
-    new_root->parent = tree->parent;
+	new_root->parent = tree->parent;
 
-    if (tree->parent != NULL)
-    {
-        if (tree->parent->left == tree)
-            tree->parent->left = new_root;
-        else
-            tree->parent->right = new_root;
-    }
+	if (tree->parent != NULL)
+	{
+		if (tree->parent->left == tree)
+			tree->parent->left = new_root;
+		else
+			tree->parent->right = new_root;
+	}
 
-    tree->parent = new_root;
-    new_root->right = tree;
+	tree->parent = new_root;
+	new_root->right = tree;
 
-    return (new_root);
+	return (new_root);
 }
