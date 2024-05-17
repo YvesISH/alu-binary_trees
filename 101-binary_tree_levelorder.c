3fa26f6e479ce1f6f298aaebefdd9c8e7737e208
FrankOnyemaOrji <f.orji@alustudent.com>
2024-05-17 11:07:48 +0200

    New changes done

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_levelorder - Goes through a binary tree
- * using level-order traversal
+ * using level-order traversal tree
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  *
